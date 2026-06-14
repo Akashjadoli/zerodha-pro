0504d41e45caf009aede8f46da2d9401b160911c
akashjadoli <suryavanshiaksh1958@gmail.com>
2026-06-14 20:32:53 +0530

    fix login url

--- frontend/src/landing_page/login/Login.js
+++ frontend/src/landing_page/login/Login.js
@@ -10,7 +10,7 @@ function Login() {
     const handleLogin = async (e) => {
         e.preventDefault();
         try {
-            const res = await axios.post("https://zerodha-pro.onrender.com2/api/auth/login", {
+            const res = await axios.post("https://zerodha-pro.onrender.com/api/auth/login", {
                 email,
                 password
             });
